2d3426041083272a61bb9dd19dcc6c2eb90f8fd1
Daniel Nunes Teixeira <nunesteixeiradaniel12@gmail.com>
2025-09-16 21:15:03 -0300

    First implementation of multi-paging.

--- src/request_handler.c
+++ src/request_handler.c
@@ -1,5 +1,6 @@
 
 #include <sys/stat.h>
+#include <stdbool.h>
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
@@ -24,8 +25,7 @@ static const char *get_filename_ext(const char *filename) {
 }
 
 static char *get_filename(const char *filename) {
-    char *last_extension;
-    char *result;
+    char *last_extension, *result;
     if (filename == NULL) return NULL;
     if ((result = malloc(strlen (filename) + 1)) == NULL) return NULL;
     strcpy(result, filename);
@@ -55,22 +55,21 @@ static int render_content()
     DIR *directory;
     
     if ((directory = opendir(CURRENT_PROJECT_FOLDER)) == NULL) {
-	fprintf(stderr, "[FAILED] : Porject folder could not be opened.\n");
+	fprintf(stderr, "[FAILED] : Project folder could not be opened.\n");
 	return -1;
     }
     while ((directory_entry = readdir(directory)) != NULL) {
 	const char *file_ext = get_filename_ext(directory_entry->d_name);
 	
 	if (strcmp(file_ext, "rd") == 0) {
-	    // printf("[INFO] : %s\n", directory_entry->d_name);
-	    const char *templ  = file_to_char("../../index.html");
+	   // printf("[INFO] : %s\n", directory_entry->d_name);
 	    size_t file_path_length = strlen(CURRENT_PROJECT_FOLDER) + strlen(directory_entry->d_name) + 1;
 	    char *file_path;
 	    if ((file_path = (char*)malloc(file_path_length)) == NULL) { fprintf(stderr, "[FAILED] Failed to allocated memory blah blah blah.\n"); return -1; }
 	    snprintf(file_path, file_path_length, "%s%s", CURRENT_PROJECT_FOLDER, directory_entry->d_name);
-	    const char *result = translate_content(file_path);
+	    const char *templ, *result;
 	    
-	    if (templ == NULL || result == NULL) {
+	    if ((templ = file_to_char("../../index.html")) == NULL || (result = translate_content(file_path)) == NULL) {
 		fprintf(stderr, "[FAILED] : Failed to load content.\n");
 		return -1;
 	    }
@@ -108,19 +107,28 @@ int handle_client(int socket)
 {
     char buffer[BUFFER_SIZE] = { 0 };
     read(socket, buffer, BUFFER_SIZE);
-    render_content();
     
-    const char *content = file_to_char(CURRENT_PROJECT_FOLDER"bin/main.html");
+    char method[256], path[BUFFER_SIZE];
+    sscanf(buffer, "%s %s", method, path);
+    
+    const char *page = strcmp(path, "/") == 0 ? "main.html" : path + 1;
+    char file_path[BUFFER_SIZE];
+    
+    if ((render_content()) == -1) goto ERROR;
+    snprintf(file_path, sizeof(file_path), CURRENT_PROJECT_FOLDER"bin/%s", page);
+    
+    const char *content = file_to_char(file_path);
     if (content) {
 	const char *http_header = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
 	write(socket, http_header, strlen(http_header));
 	write(socket, content, strlen(content));
-    } else {
-	const char* not_found_response = "HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\n<h1>404 Not Found</h1>";
-	write(socket, not_found_response, strlen(not_found_response));
-	close(socket);
-	return -1;
-    }
+    } else goto ERROR;
+    
     close(socket);
     return 0;
+ERROR:
+    const char* not_found_response = "HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\n<h1>404 Not Found</h1>";
+    write(socket, not_found_response, strlen(not_found_response));
+    close(socket);
+    return -1;
 }
